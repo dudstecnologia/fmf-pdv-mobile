ed30e28656562b985546bada387d2e782ee8e83b
Eduardo Silva <eduardo@dudstecnologia.com.br>
2024-10-05 06:20:29 -0400

    Config icons

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button, Icon } from "@rneui/themed";
 import { connectDb, createTables } from './src/services/db'
 
 import Home from './src/pages/Home';
@@ -52,7 +53,22 @@ export default function App() {
         <Stack.Screen name="home" component={ Home } options={{ title: 'Tela Inicial' }} />
         <Stack.Screen name="testCamera" component={ TestCamera } options={{ title: 'Teste da Câmera' }} />
         <Stack.Screen name="testSqlite" component={ TestSqlite } options={{ title: 'Teste do Sqlite' }} />
-        <Stack.Screen name="productList" component={ ProductList } options={{ title: 'Produtos' }} />
+        <Stack.Screen
+          name="productList"
+          component={ ProductList }
+          options={({ navigation }) => {
+            return {
+                title: 'Lista de Usuários',
+                headerRight: () => (
+                    <Button
+                        type="clear"
+                        icon={<Icon name="add" size={25} color="white" />}
+                        onPress={() => navigation.navigate('UserForm')}
+                    />
+                )
+            }
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
